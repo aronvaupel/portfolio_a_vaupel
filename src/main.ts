57c0343383be451a2f875e38ff67bd247aa07d3e
aron vaupel <vaupelaron@gmail.com>
2022-03-06 17:09:06 +0100

    is this the first commit???? Who knows...

---
+++ src/main.ts
@@ -0,0 +1,83 @@
+const aron = document.getElementById("aron") as HTMLElement;
+const vaupel = document.getElementById("vaupel") as HTMLElement;
+const flipCard = document.getElementById("flip-card") as HTMLElement;
+const flipCardInner = document.getElementById("flip-card-inner") as HTMLElement;
+const flipCardFront = document.getElementById("flip-card-front") as HTMLElement;
+const flipCardBack = document.getElementById("flip-card-back") as HTMLElement;
+const profilePicture = document.getElementById(
+  "profile-picture"
+) as HTMLElement;
+const flipCardText = document.getElementById("flip-card-text") as HTMLElement;
+const header = document.getElementById("header") as HTMLElement;
+
+let state: number = 0;
+
+function scroll(event: WheelEvent): void {
+  const moveUp = `translateY(-${state < 10 ? state * 5 : 50}vh)`;
+  const moveDown = `translateY(${state < 10 ? state * 5 : 50}vh)`;
+  const slide = `translateX(calc(${state <= 10 ? state * 5 : 50}vw - 12.5vw))`;
+  const roll = `rotateZ(${state <= 10 ? state * 36 : 0}deg)`;
+  const grow = `${state <= 5 ? state * 10 : 50}vh`;
+  const growText = `${state < 10 ? state * 0.1 : 1}rem`;
+  const headerAppear = `translateY(${state < 10 ? 0 : 11}vh)`;
+  const opacityIncrease = `${state < 10 ? state / 10 : 1}`;
+  const opacityDecrease = `${state < 10 ? 1 - state / 10 : 0}`;
+
+  if (state > 0 && event.deltaY < 0) {
+    state -= 1;
+    flipCard.removeEventListener(
+      "mouseenter",
+      () => (flipCardInner.style.transform = "rotateY(180deg)")
+    );
+    flipCard.removeEventListener(
+      "mouseleave",
+      () => (flipCardInner.style.transform = "rotateY(360deg)")
+    );
+    console.log("goodbye");
+  } else if (state < 10 && event.deltaY > 0) {
+    state += 1;
+    flipCard.removeEventListener(
+      "mouseenter",
+      () => (flipCardInner.style.transform = "rotateY(180deg)")
+    );
+    flipCard.removeEventListener(
+      "mouseleave",
+      () => (flipCardInner.style.transform = "rotateY(360deg)")
+    );
+    console.log("goodbye");
+  } else {
+    flipCard.addEventListener(
+      "mouseenter",
+      () => (flipCardInner.style.transform = "rotateY(180deg)")
+    );
+    flipCard.addEventListener(
+      "mouseleave",
+      () => (flipCardInner.style.transform = "rotateY(360deg)")
+    );
+    console.log("hello");
+  }
+
+  aron.style.transform = moveUp;
+  aron.style.opacity = opacityDecrease;
+  vaupel.style.transform = moveDown;
+  vaupel.style.opacity = opacityDecrease;
+  flipCard.style.transform = slide;
+  flipCardInner.style.transform = roll;
+  flipCard.style.width = grow;
+  flipCard.style.height = grow;
+  flipCardInner.style.width = grow;
+  flipCardInner.style.height = grow;
+  flipCardFront.style.width = grow;
+  flipCardFront.style.height = grow;
+  flipCardBack.style.width = grow;
+  flipCardBack.style.height = grow;
+  profilePicture.style.width = grow;
+  profilePicture.style.height = grow;
+  flipCardText.style.fontSize = growText;
+  header.style.transform = headerAppear;
+  header.style.opacity = opacityIncrease;
+
+  console.log(state);
+}
+
+window.addEventListener("wheel", scroll);
